a9551d01a78cc0fc9ac8ac3db278b68c28f14346
khs <khs@bihand.co.kr>
2012-04-20 21:06:05 +0900

    sdiff, space 테스트 추가

--- lib/Template/Reverse.pm
+++ lib/Template/Reverse.pm
@@ -2,4 +2,57 @@ use strict;
 use warnings;
 package Template::Reverse;
 # ABSTRACT: turns baubles into trinkets
+use Algorithm::Diff qw(LCS LCS_length LCSidx diff sdiff compact_diff traverse_sequences traverse_balanced);
+sub dd{
+    my ($a,$b) = @_;
+
+    my @d = sdiff($a,$b);
+    my @rr;
+    my $before='';
+    for my $r (@d){
+        if( $r->[0] eq 'u' ){
+            push(@rr,$r->[1]);
+            $before = '';
+        }
+        else{
+            push(@rr,'*') if( $before ne '*' );
+            $before = '*';
+        }
+    }
+    return \@rr;
+}
+
+sub makeMap{
+    my @asc = qw(a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 );
+    my $str = shift;
+    my $spaced = space($str);
+    my @chunk = split(/\s+/,$spaced);
+
+    my $out='';
+    foreach my $c (@chunk)
+    {
+        my $sum=checksum($c);
+        $out.= $asc[$sum % @asc];
+    }
+    return $out;
+}
+
+sub space{
+    my $str = shift;
+    $str =~ s/(\d[\d,]*(\.\d+)?)(\D)/$1 $3/gm;
+    $str =~ s/([^,.\d])(\d[\d,]*(\.\d+)?)/$1 $2/gm;
+    $str =~ s/\s+/ /g;
+    $str =~ s/^\s//g;
+    $str =~ s/\s$//g;
+    return $str;
+}
+
+sub checksum{
+    my $str = shift;
+    my $sum = 0;
+    map{$sum+=ord($_)}split(//,$str);
+    return $sum;
+}
+
+
 1;
---
+++ t/00-sdiff.t
@@ -0,0 +1,51 @@
+use Test::More ;
+use Data::Dumper;
+
+BEGIN{
+};
+
+use Template::Reverse;
+
+sub dd{
+    my ($a,$b) = @_;
+    return Template::Reverse::dd($a,$b);
+}
+
+my (@seq1,@seq2,@exp,$diff);
+
+@seq1 = qw( A B C D E F );
+@seq2 = qw( A B C D E F );
+@exp  = qw( A B C D E F );
+$diff = dd(\@seq1,\@seq2);
+print Dumper($diff);
+ok eq_array($diff, \@exp), 'sdiff test';
+
+@seq1 = qw( A B C D E F );
+@seq2 = qw( A B C   E F );
+@exp  = qw( A B C * E F );
+$diff = dd(\@seq1,\@seq2);
+print Dumper($diff);
+ok eq_array($diff, \@exp), 'sdiff test';
+
+@seq1 = qw( A B C D E F );
+@seq2 = qw( A B     E F );
+@exp  = qw( A B * E F );
+$diff = dd(\@seq1,\@seq2);
+print Dumper($diff);
+ok eq_array($diff, \@exp), 'sdiff test';
+
+@seq1 = qw( A B C D E F );
+@seq2 = qw( B     E F );
+@exp  = qw( * B * E F );
+$diff = dd(\@seq1,\@seq2);
+print Dumper($diff);
+ok eq_array($diff, \@exp), 'sdiff test';
+
+@seq1 = qw( A B C D E F );
+@seq2 = qw(   B C D E );
+@exp  = qw( * B C D E * );
+$diff = dd(\@seq1,\@seq2);
+print Dumper($diff);
+ok eq_array($diff, \@exp), 'sdiff test';
+
+done_testing();
